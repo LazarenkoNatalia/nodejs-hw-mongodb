b8af08c09d453cb5bcd8fc86cdf51353e4b42242
LazarenkoNatalia <lazarenkon.n.v@gmail.com>
2024-08-17 21:04:25 +0300

    dz3

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -5,7 +5,31 @@ export const getAllContacts = async () => {
   return contacts;
 };
 
-export const getContactById = async (contactId) => {
-  const contact = await ContactsCollection.findById(contactId);
+export const getContactById = async (id) => {
+  const contact = await ContactsCollection.findById(id);
   return contact;
+};
+
+export const createContact = async (payload) => {
+  return await ContactsCollection.create(payload);
+};
+
+export const updateContact = async (id, payload, options = {}) => {
+  const contactForUpdate = await ContactsCollection.findOneAndUpdate(
+    { _id: id },
+    payload,
+    {
+      new: true,
+      includeResultMetadata: true,
+      ...options,
+    },
+  );
+  if (!contactForUpdate || !contactForUpdate.value) return null;
+  return {
+    contact: contactForUpdate.value,
+  };
+};
+
+export const deleteContact = async (id) => {
+  return await ContactsCollection.findOneAndDelete({ _id: id });
 };
